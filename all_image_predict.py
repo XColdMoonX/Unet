18e5a5a77d1b171d8d835e3e2d342faa45a9a1e8
JonasFu <jasmine21153126@gmail.com>
2024-06-25 17:44:03 +0800

    2024/06/25

--- all_image_predict.py
+++ all_image_predict.py
@@ -7,18 +7,22 @@ import subprocess
 # InputDir = "D:\\Jonas\\RiverData\\301"  # 輸入資料夾
 # OutputDir = "D:\\Jonas\\RiverData\\301_Pre"  # 輸出資料夾
 
+from tqdm import tqdm
+
 def all_predict(PredictPath, ModelPath, InputDir, OutputDir):
     # 獲取輸入資料夾中的所有.jpg檔案
     input_files = [f for f in os.listdir(InputDir) if f.endswith('.jpg')]
 
-    for file in input_files:
+    # 使用tqdm產生進度條
+    for file in tqdm(input_files, desc="Processing files"):
         print(f"Processing file: {file}")  # 顯示目前正在處理的檔案
         InputPath = os.path.join(InputDir, file)  # 完整的輸入檔案路徑
-        OutputPath = os.path.join(OutputDir, file)  # 完整的輸出檔案路徑
+        OutputFileName = os.path.splitext(file)[0] + '.png'  # 將輸出檔案的副檔名改為.png
+        OutputPath = os.path.join(OutputDir, OutputFileName)  # 完整的輸出檔案路徑
 
         # 檢查輸出資料夾中是否已經存在相同名稱的檔案，若有，則跳過這個檔案
         if os.path.exists(OutputPath):
-            print(f"File {file} already exists in output directory. Skipping...")
+            print(f"File {OutputFileName} already exists in output directory. Skipping...")
             continue
 
         # 執行 "python predict.py -m ModelPath -i InputPath -o OutputPath" 指令
@@ -26,3 +30,24 @@ def all_predict(PredictPath, ModelPath, InputDir, OutputDir):
                         ModelPath, "-i",
                         InputPath, "-o",
                         OutputPath], check=True)
+        
+# def all_predict(PredictPath, ModelPath, InputDir, OutputDir):
+#     # 獲取輸入資料夾中的所有.jpg檔案
+#     input_files = [f for f in os.listdir(InputDir) if f.endswith('.jpg')]
+
+#     for file in input_files:
+#         print(f"Processing file: {file}")  # 顯示目前正在處理的檔案
+#         InputPath = os.path.join(InputDir, file)  # 完整的輸入檔案路徑
+#         OutputFileName = os.path.splitext(file)[0] + '.png'  # 將輸出檔案的副檔名改為.png
+#         OutputPath = os.path.join(OutputDir, OutputFileName)  # 完整的輸出檔案路徑
+
+#         # 檢查輸出資料夾中是否已經存在相同名稱的檔案，若有，則跳過這個檔案
+#         if os.path.exists(OutputPath):
+#             print(f"File {OutputFileName} already exists in output directory. Skipping...")
+#             continue
+
+#         # 執行 "python predict.py -m ModelPath -i InputPath -o OutputPath" 指令
+#         subprocess.run(["python", PredictPath, "-m",
+#                         ModelPath, "-i",
+#                         InputPath, "-o",
+#                         OutputPath], check=True)
--- final_auto_matching.py
+++ final_auto_matching.py
@@ -1,6 +1,7 @@
 from all_image_predict import all_predict
 from mask_dilation import dilation
 from mask_to_make_color_river import mask_cut_image
+from concat_pic import handleMulti
 
 PredictPath = "D:\\Jonas\\PythonSpace\\Pytorch-UNet\\predict.py" #predict.py的路徑
 ModelPath = "D:\\Jonas\\PythonSpace\\checkpoint\\1_checkpoint_epoch1.pth" # model.pth的路徑
@@ -13,4 +14,5 @@ CutDir = "D:\\Jonas\\RiverData\\301_Pre_Cut"  # 輸出切割的資料夾
 
 all_predict(PredictPath,ModelPath,OriginDir,MaskDir)    #  all_predict(PredictPath,ModelPath,InputDir,OutputDir)
 dilation(MaskDir,DilationDir)                           #  dilation(input_folder,output_folder)
-mask_cut_image(DilationDir,OriginDir,MaskDir)           #  mask_cut_image(mask_folder, image_folder, output_folder)
+mask_cut_image(DilationDir,OriginDir,CutDir)           #  mask_cut_image(mask_folder, image_folder, output_folder)
+#handleMulti()                                           #  handleMulti(放入多張圖片)
