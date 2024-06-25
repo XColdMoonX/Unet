2d5371ef62bc56cc24df7ded345eaff17d8dd2e4
JonasFu <jasmine21153126@gmail.com>
2024-06-25 15:36:19 +0800

    檢查是否存在已經的遮罩

--- all_image_predict.py
+++ all_image_predict.py
@@ -7,15 +7,19 @@ import subprocess
 # InputDir = "D:\\Jonas\\RiverData\\301"  # 輸入資料夾
 # OutputDir = "D:\\Jonas\\RiverData\\301_Pre"  # 輸出資料夾
 
-def all_predict(PredictPath,ModelPath,InputDir,OutputDir):
+def all_predict(PredictPath, ModelPath, InputDir, OutputDir):
     # 獲取輸入資料夾中的所有.jpg檔案
     input_files = [f for f in os.listdir(InputDir) if f.endswith('.jpg')]
 
-    # 使用tqdm來創建進度條
     for file in input_files:
         print(f"Processing file: {file}")  # 顯示目前正在處理的檔案
         InputPath = os.path.join(InputDir, file)  # 完整的輸入檔案路徑
         OutputPath = os.path.join(OutputDir, file)  # 完整的輸出檔案路徑
+
+        # 檢查輸出資料夾中是否已經存在相同名稱的檔案，若有，則跳過這個檔案
+        if os.path.exists(OutputPath):
+            print(f"File {file} already exists in output directory. Skipping...")
+            continue
 
         # 執行 "python predict.py -m ModelPath -i InputPath -o OutputPath" 指令
         subprocess.run(["python", PredictPath, "-m",
