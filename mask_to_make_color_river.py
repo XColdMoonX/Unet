58e80275cfe7210b77b23bbdd804655fcbab9ec9
XColdMoonX <139200526+XColdMoonX@users.noreply.github.com>
2024-07-11 16:26:37 +0800

    Update mask_to_make_color_river.py
    調整檢測是否存在的位置，加快運行速度

--- mask_to_make_color_river.py
+++ mask_to_make_color_river.py
@@ -19,14 +19,15 @@ def mask_cut_image(mask_folder, image_folder, output_folder):
 
     # 遍歷每一個遮罩和圖片
     for mask_file, image_file in zip(mask_files, image_files):
-        # 讀取遮罩和圖片
-        mask = cv2.imread(os.path.join(mask_folder, mask_file), cv2.IMREAD_GRAYSCALE)
-        image = cv2.imread(os.path.join(image_folder, image_file))
-        # 若輸出圖片已經存在，則跳過
+         # 若輸出圖片已經存在，則跳過
         if os.path.exists(os.path.join(output_folder, image_file)):
             print(f"File {image_file} already exists in output directory. Skipping...")
             continue
-        
+            
+        # 讀取遮罩和圖片
+        mask = cv2.imread(os.path.join(mask_folder, mask_file), cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(os.path.join(image_folder, image_file))
+       
         # 確保遮罩和圖片的尺寸相同
         assert mask.shape == image.shape[:2], "Mask and image must have the same dimensions"
 
